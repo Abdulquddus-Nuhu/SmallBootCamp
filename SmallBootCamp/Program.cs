e92e633bdb25245d72d6b8f312bf11e55cd1caa6
Abdulquddus-Nuhu <abdulquddusnuhu@gmail.com>
2023-01-17 14:59:47 +0100

    add response caching

--- SmallBootCamp/Program.cs
+++ SmallBootCamp/Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+//Add Response Caching
+builder.Services.AddResponseCaching();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,6 +15,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseResponseCaching();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
